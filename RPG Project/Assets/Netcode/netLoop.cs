8d51f7e233b22297cb2404c2a4ea388cbd483180
tylernolan <trynetend@gmail.com>
2016-12-09 16:46:04 -0500

    added fields to manage a text box

--- RPG Project/Assets/Netcode/netLoop.cs
+++ RPG Project/Assets/Netcode/netLoop.cs
@@ -1,10 +1,14 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
 
 public class netLoop : MonoBehaviour {
 	private GameObject player = null;
 	public GameObject skeleton;
 	public GameObject skeletonMage;
+	public GameObject[] LogTextBoxes;
+	public Queue<string> commandLog = new Queue<string>();
 	// Use this for initialization
 	void Start () {
 		NetworkModel.sendConnRequest();
@@ -25,39 +29,58 @@ public class netLoop : MonoBehaviour {
 			executeCommand(name, command);
 		}
 	}
+	public void setCommandLog(string name, string commandText)
+	{
+		string commandStr =  name + commandText;
+		Debug.Log(commandStr);
+		commandLog.Enqueue(commandStr);
+		string[] arr = commandLog.ToArray();
+		
+		for (int i = 0; i < LogTextBoxes.Length & i < arr.Length; i++)
+		{
+			LogTextBoxes[i].GetComponent<Text>().text = arr[i];
+		}
+		if (LogTextBoxes.Length <= arr.Length)
+		{
+			commandLog.Dequeue();
+		}
+		
+	}
 	/**
 	0: Summon Basic Skeleton
 	1: Summon Skeleton Mage
 	2: Heal player 10
 	3: Give player 10 mana
+	4: Smite an enemy
 	**/
 	public void executeCommand(string name, int command)
 	{
+		
 		if (command == 0)
 		{
 			Instantiate(skeleton, player.transform.position, player.transform.rotation);
-			Debug.Log(name+" has summoned a Skeleton!");
+			setCommandLog(name, " Has summoned a Skeleton!");
 		}
 		else if (command == 1)
 		{
 			Instantiate(skeletonMage, player.transform.position, player.transform.rotation);
-			Debug.Log(name+" has summoned a Mage!");
+			setCommandLog(name, " Has summoned a Mage!");
 		}
 		else if (command == 2)
 		{
 			player.GetComponent<Fighter>().Get_Hit(-10); //hitting for negative values heals
-			Debug.Log(name+" has healed the player!");
+			setCommandLog(name, " Has healed the player!");
 		}
 		else if (command == 3)
 		{
 			player.GetComponent<Fighter>().Use_Mana(-10);
-			Debug.Log(name+" has given the player mana!");
+			setCommandLog(name, " Has given the player mana!");
 		}
 		else if (command == 4)
 		{
 			GameObject enemy = GameObject.FindGameObjectsWithTag("Enemy")[0];
 			enemy.GetComponent<mob>().smite();
-			Debug.Log(name+" has smited an enemy!");
+			setCommandLog(name, " Has smited an enemy!");
 		}
 	}
 }
