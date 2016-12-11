5727b8dc598881d3c9ca4684effeb6dfffd3415e
TomMGrello <tommgrello@gmail.com>
2016-12-11 02:18:24 -0500

    Player no longer stupidly walks into walls. (Make sure any new props that you don't want to impede the player are tagged. The move script makes the player halt when it hits an object that's Untagged.) Also started a weird hell-like level design.

--- RPG Project/Assets/Scripts/ClickToMove.cs
+++ RPG Project/Assets/Scripts/ClickToMove.cs
@@ -11,7 +11,7 @@ public class ClickToMove : MonoBehaviour {
 	public static bool attack = false;
 	public static bool dieing = false;
 	public GameObject particleEffect;
-	
+	private bool useParticle = true;
 
 	// Use this for initialization
 	void Start () 
@@ -28,8 +28,12 @@ public class ClickToMove : MonoBehaviour {
 		if (!attack&&!dieing) 
 		    {
 			if (Input.GetMouseButton (0)) {
-					//locate where the player clicked on the terain 
-					Locate_Position ();
+				//locate where the player clicked on the terain 
+
+				Locate_Position ();
+			} else {
+				useParticle = true;
+
 			}
 
 			MoveToPosition ();
@@ -50,7 +54,10 @@ public class ClickToMove : MonoBehaviour {
 		{
 			if (hit.collider.tag == "Floor") {
 				position = new Vector3 (hit.point.x, hit.point.y, hit.point.z);
-				Instantiate (particleEffect, position, Quaternion.identity);
+				if (useParticle) {
+					Instantiate (particleEffect, position, Quaternion.identity);
+					useParticle = false;
+				}
 			}
 		}
 	}
@@ -59,16 +66,18 @@ public class ClickToMove : MonoBehaviour {
 	void MoveToPosition()
 	{ 
 		//when game object is moving 
-		if (Vector3.Distance (transform.position, position) > 1) {
-						Quaternion newRotation = Quaternion.LookRotation (position - transform.position);
-						newRotation.z = 0f;
+		if (Vector3.Distance (transform.position, position) > 1 && canMove()) {
+			if (canMove ()) {
+				Quaternion newRotation = Quaternion.LookRotation (position - transform.position);
+				newRotation.z = 0f;
 
-						transform.rotation = Quaternion.Slerp (transform.rotation, newRotation, Time.deltaTime * 10);
-						//transform.position += transform.forward * speed * Time.deltaTime;
-						CharControl.SimpleMove (transform.forward * speed );
+				transform.rotation = Quaternion.Slerp (transform.rotation, newRotation, Time.deltaTime * 10);
+				//transform.position += transform.forward * speed * Time.deltaTime;
+				CharControl.SimpleMove (transform.forward * speed);
 						
-						GetComponent<Animation>().CrossFade(run.name);
-				} 
+				GetComponent<Animation> ().CrossFade (run.name);
+			}
+		} 
 		else 
 		{
 
@@ -76,7 +85,21 @@ public class ClickToMove : MonoBehaviour {
 		}
 	}
 
+	bool canMove() {
+		Collider[] cols = Physics.OverlapSphere (transform.position, 1);
+		foreach(Collider col in cols) {
+			if (col.gameObject.tag == "Untagged") {
+				Knockback ();
+				return false;
+			}
+		}
+		return true;
+	}
 
+	void Knockback() {
+		CharControl.SimpleMove (transform.forward * -2);
+		position = transform.position;
+	}
 
 }
 
